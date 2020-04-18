299308362509dd139cea10d3060a9ea64a5b2209
ary <arypontys@hotmail.com>
2020-04-17 22:29:18 -0300

    Commit inicial do projeto Crud no Intellij

---
+++ src/main/java/main/ArticleController.java
@@ -0,0 +1,7 @@
+package main;
+/*
+ * Created by Arimateia Pontes on 17/04/2020
+ */
+
+public class ArticleController {
+}
---
+++ src/main/java/main/MainController.java
@@ -0,0 +1,14 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+* Created by Arimateia Pontes on 17/04/2020
+*/
+@SpringBootApplication
+public class MainController {
+    public static void main(String[] args){
+        SpringApplication.run(MainController.class,args);
+    }
+}
