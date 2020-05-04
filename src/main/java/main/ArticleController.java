483c95ee5adf6fbd38825fc2bc4b40a893188a40
ary <arypontys@hotmail.com>
2020-05-04 00:48:14 -0300

    8º WIP Commit do projeto Crud no Intellij desenvolvido os metodos update e delete.

--- src/main/java/main/ArticleController.java
+++ src/main/java/main/ArticleController.java
@@ -19,14 +19,26 @@ public class ArticleController {
     {
         return articleService.getAllArticles();
     }
+
     @RequestMapping("/articles/(id)")
     public Article getArticle(@PathVariable String id)
     {
         return articleService.getArticle(id);
     }
+
     @RequestMapping(method = RequestMethod.POST, value = "/articles")
     public void addArticle(@RequestBody Article article)
     {
         articleService.addArticle(article);
+    }
+    @RequestMapping(method = RequestMethod.PUT, value = "/articles/{id}")
+    public void updateArticle(@RequestBody Article article, @PathVariable String id)
+    {
+        articleService.updateArticle(article, id);
+    }
+    @RequestMapping(method = RequestMethod.DELETE, value = "/articles/{id}")
+    public void deleteArticle(@PathVariable String id)
+    {
+        articleService.deleteArticle(id);
     }
 }
